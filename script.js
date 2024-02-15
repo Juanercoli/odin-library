4d4e37f4c71cee959efabf4f5c9bb92c992f270c
Juan Ercoli <ercoli.juan@alu.ing.unlp.edu.ar>
2024-02-15 20:53:23 -0300

    finish

---
+++ script.js
@@ -0,0 +1,71 @@
+// Modal related
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("aside > button");
+const closeButton = document.querySelector("dialog button");
+const createButton = document.querySelector("#createBtn"); 
+
+createButton.addEventListener("click", (e) => {
+	const title = document.querySelector("#title").value;
+	const author = document.querySelector("#author").value;
+	const pages = document.querySelector("#pages").value;
+	const read = document.querySelector("#read").value; 
+	const book = new Book(title, author, pages, read); 
+	console.log
+	addBookToLibrary(book);
+	dialog.close();
+	displayBooks();
+	e.preventDefault();
+});
+
+showButton.addEventListener("click", () => {
+	dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+	dialog.close();
+});
+
+// Library related
+
+const book1 = new Book('The Lord of the Rings', 'J.R.R Tolkien', 1356, false);
+const book2 = new Book('The Lord of the Rings II', 'J.R.R Tolkien', 1326, true);
+const book3 = new Book('The Lord of the Rings III', 'J.R.R Tolkien', 1556, false);
+
+const myLibrary = [book1, book2, book3];
+
+addBookToLibrary(book1);
+displayBooks();
+
+function Book(title, author, pages, read) {
+  // the object constructor
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+}
+
+function displayBooks() {
+	document.getElementById("main").innerHTML = "";
+	myLibrary.forEach((element, index) => {
+	  const	deleteBtn =	document.createElement("button");
+		deleteBtn.textContent = "Delete book";
+		deleteBtn.addEventListener("click", () => {
+			myLibrary.splice(index, 1);
+			displayBooks();
+			alert(JSON.stringify(element));
+		});
+
+		const bookDiv = document.createElement("div");
+		console.log(element);
+		bookDiv.innerText = JSON.stringify(element);
+		bookDiv.appendChild(deleteBtn);
+		document.getElementById("main").appendChild(bookDiv);
+
+
+	});
+}
+
+function addBookToLibrary(book) {
+	myLibrary.push(book);
+}
+
